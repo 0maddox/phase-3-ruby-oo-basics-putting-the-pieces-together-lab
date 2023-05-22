cbe2af70e1c723da404752b1ced7bd89029383ad
nicholas <nickkiim7@gmail.com>
2023-05-22 07:59:05 +0300

    complete  lab

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,11 +1,44 @@
 class Shoe
-    attr_accessor  :color,:size,:material,:condition
-    attr_reader :brand
     def initialize(brand)
-        @brand=brand
+        @brand = brand
+    end
+    def brand=(brand)
+        @brand = brand
+    end
+    def brand
+        @brand
+    end
+    def color=(color)
+        @color = color
+    end
+    def color
+        @color
+    end
+    def size=(size)
+        @size = size
+    end
+    def size
+        @size
+    end
+    def material=(material)
+        @material = material
+    end
+    def material
+        @material
+    end
+    def condition=(condition)
+        @condition = condition
+    end
+    def condition
+        @condition
     end
     def cobble
-        @condition = "new"
         puts "Your shoe is as good as new!"
-      end
+        @condition ="new"
+
+    end
+
+
 end
+
+shoe = Shoe.new("Adidas")
